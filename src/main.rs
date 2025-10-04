ffb2bbeebfdb768c464f4bc8c7c8f9cadda1644c
Dmitry Seloustev <nimec77@gmail.com>
2025-10-04 13:29:23 +0700

    Add progress display functionality to main logic and utility helpers

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod utils;
 
 use config::DEFAULT_COURSE_NAME;
 use time::OffsetDateTime;
-use utils::helpers::greet;
+use utils::helpers::{greet, show_progress};
 
 fn main() {
     greet();
@@ -11,4 +11,7 @@ fn main() {
     println!("Сегодня: {}", OffsetDateTime::now_utc().date());
 
     println!("Я прохожу курс: {}!", DEFAULT_COURSE_NAME);
+
+    println!("Мой прогресс в текущем модуле:");
+    show_progress(5, 15);
 }
